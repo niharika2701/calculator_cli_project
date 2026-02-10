cefe9ed5d712352ff89c096a95ebeaf31827ca64
Niharika Jadhav <niharika.jadhav03@gmail.com>
2026-02-09 22:50:47 -0500

    Refactoring tests to use parameterization aka DRY principle

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -6,49 +6,54 @@ class TestCalculator:
 
         self.calc=Calculator()
 
-    def test_add_two_positive_numbers(self):
-        
-        assert self.calc.add(2,3) == 5
-    
-    def test_add_two_negative_numbers(self):
-        
-        assert self.calc.add(-2,-3) == -5
-    
-    def test_add_two_mixed_signs(self):
-        
-        assert self.calc.add(-5,3) == -2
-    
-    def test_subtract_positive_numbers(self):
-        
-        assert self.calc.subtract(5, 3) == 2
-    
-    def test_subtract_negative_result(self):
-        
-        assert self.calc.subtract(3, 5) == -2
+    @pytest.mark.parametrize("a, b, expected", [
+        (2, 3, 5),           
+        (-2, -3, -5), 
+        (-5, 3, -2),         
+        (0, 0, 0),           
+        (100, -100, 0),      
+        (0.1, 0.2, 0.3),
+    ])
 
-    def test_multiply_positive_numbers(self):
-        
-        assert self.calc.multiply(4, 3) == 12
+    def test_add(self, a, b, expected):
+        assert self.calc.add(a, b) == pytest.approx(expected)
+
+    @pytest.mark.parametrize("a, b, expected", [
+        (5, 3, 2),
+        (3, 5, -2),
+        (0, 5, -5),
+        (-3, -5, 2),
+    ])
+
+    def test_subtract(self, a, b, expected):
+        assert self.calc.subtract(a, b) == expected
     
-    def test_multiply_by_zero(self):
-        
-        assert self.calc.multiply(5, 0) == 0
+    @pytest.mark.parametrize("a, b, expected", [
+        (4, 3, 12),
+        (5, 0, 0),
+        (0, 5, 0),
+        (-3, -4, 12),
+        (-3, 4, -12),
+    ])
+
+    def test_multiply(self, a, b, expected):
+        assert self.calc.multiply(a, b) == expected
     
-    def test_multiply_negative_numbers(self):
-        
-        assert self.calc.multiply(-3, -4) == 12
-    
-    def test_divide_positive_numbers(self):
-        
-        assert self.calc.divide(10, 2) == 5
-    
-    def test_divide_results_in_float(self):
-        
-        assert self.calc.divide(5, 2) == 2.5
+    @pytest.mark.parametrize("a, b, expected", [
+        (10, 2, 5),
+        (5, 2, 2.5),
+        (-10, 2, -5),
+        (10, -2, -5),
+    ])
+
+    def test_divide(self, a, b, expected):
+        assert self.calc.divide(a, b) == expected
     
     def test_divide_by_zero_raises_error(self):
-        
         with pytest.raises(ValueError, match="Cannot divide by zero"):
-            self.calc.divide(5, 0)
+            self.calc.divide(5,0)
+    
+    
+
 
 
